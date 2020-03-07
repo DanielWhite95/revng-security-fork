40b9cbd506ca6391c085030c847e142eacebb5d3
Daniele Marsella <daniele.marsella95@gmail.com>
2020-03-07 14:55:00 +0100

    Minor changes

--- lib/SecurityPass/CommonDefinitions.cpp
+++ lib/SecurityPass/CommonDefinitions.cpp
@@ -4,4 +4,4 @@
 using namespace llvm;
 using namespace revng;
 
-cl::opt<bool, true> OnlyMarkedFunctions("only-marked-funs", cl::desc("Analyze only functions reached by inputs"), cl::location(only_marked_funs));
+cl::opt<bool> OnlyMarkedFunctions("only-marked-funs", cl::desc("Analyze only functions reached by inputs"));
