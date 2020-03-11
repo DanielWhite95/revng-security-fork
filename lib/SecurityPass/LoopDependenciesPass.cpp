434699acefa592dcad36d768c9342482ccf50de9
Daniele Marsella <daniele.marsella95@gmail.com>
2020-03-11 12:08:04 +0100

    Updated support libraries

--- lib/SecurityPass/LoopDependenciesPass.cpp
+++ lib/SecurityPass/LoopDependenciesPass.cpp
@@ -47,7 +47,7 @@ bool LoopDependenciesPass::runOnFunction(Function &F) {
 	  LDPSkippedFunctions++;
 	  errs() << "Not a function in the scope of analysis, skipping...\n" ;
 	  return false;
-	}if (!isMarked(&F) && only_marked_funs) {
+	}if (!isMarked(&F) && OnlyMarkedFunctions) {
 		LDPSkippedFunctions++;
 		errs() << "Function not reached by any input, skipping analysis ...\n";
 		return false;
